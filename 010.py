8160a00895b5267c1a5e11785116bed870ada86f
Gerald Haxhillari <32375141+GeraldHaxhillari@users.noreply.github.com>
2018-12-26 15:50:30 +0100

    Create 010.py

---
+++ 010.py
@@ -0,0 +1,34 @@
+"""
+Summation of primes
+Problem 10 
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+
+import numpy as np
+
+n = 5000000
+sieve = np.array([True for _ in range(n)])
+sieve[0:2] = False
+upper_bound = int(np.sqrt(n))
+
+for i in range(2, upper_bound+1):
+    if sieve[i]:
+        j = i * i
+        counter = 0
+        while j < n:
+            sieve[j] = False
+            j = (i * i) + (counter * i)
+            counter += 1
+
+primes = np.where(sieve == True)[0]
+
+idx = np.where(primes < 2000000)[0]
+subset = primes[idx]
+solution = sum(subset)
+print('solution: ', solution)
+
+"""
+142913828922
+"""
